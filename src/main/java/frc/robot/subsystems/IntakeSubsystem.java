39cdff6617c3cc53bb9c76a1420f130c2430f5c8
theachan0032 <hilttheo0032@apps.hopkinsschools.org>
2021-03-13 22:31:55 -0600

    removed bloat

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -6,28 +6,12 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase {
     private final WPI_TalonFX motor;
-    private double lastSpeed;
 
     public IntakeSubsystem(int motorID) {
         this.motor = new WPI_TalonFX(motorID);
-        this.lastSpeed = 0;
     }
-
-    public void spin() {
-        motor.set(lastSpeed);
-    } // Potential idea: Call spin() on teleop Periodic, and modify lastSpeed when
-      // certain buttons are pressed.
 
     public void spin(double newSpeed) {
-        this.setSpeed(newSpeed);
-        this.spin();
-    }
-
-    public void setSpeed(double newSpeed) {
-        this.lastSpeed = newSpeed;
-    }
-
-    public double getSpeed() {
-        return this.lastSpeed;
+        motor.set(newSpeed);
     }
 }
