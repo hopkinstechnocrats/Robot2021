47d238dd63b4e47cdd6ce92ebf8111ecfa9676af
EliteBook 8770w <henry.scheible@gmail.com>
2021-03-23 13:22:47 -0500

    add intake code

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,22 @@
+package frc.robot.subsystems;
+
+import javax.sound.midi.VoiceStatus;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IntakeConstants;
+
+public class IntakeSubsystem extends SubsystemBase{
+    WPI_TalonFX Motor;
+
+    public IntakeSubsystem() {
+        Motor = new WPI_TalonFX(IntakeConstants.MotorCANID);
+    }
+
+    public void spin(double speed) {
+        Motor.set(speed);
+    }
+}
