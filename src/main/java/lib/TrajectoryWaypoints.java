294e28e733807c412abd5eb32c1fe494b7cfbf04
EliteBook 8770w <henry.scheible@gmail.com>
2021-03-31 19:52:52 -0500

    fix trajectory issues

--- src/main/java/lib/TrajectoryWaypoints.java
+++ src/main/java/lib/TrajectoryWaypoints.java
@@ -6,9 +6,19 @@ public class TrajectoryWaypoints {
     double[][] waypoints;
     boolean reversed;
 
+    public TrajectoryWaypoints() {
+        start = new PoseInfo();
+        finish = new PoseInfo();
+    }
+
     class PoseInfo {
         double x;
         double y;
         double theta;
+        public PoseInfo() {
+            x = 0;
+            y = 0;
+            theta = 0;
+        }
     }
 }
