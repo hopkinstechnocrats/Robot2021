ee06fd2a451a3cb835a10977156ad6ae941fc815
EliteBook 8770w <henry.scheible@gmail.com>
2021-04-24 10:55:33 -0500

    added functionality to import pathweaver.json

--- src/main/java/lib/TrajectoryCommandGenerator.java
+++ src/main/java/lib/TrajectoryCommandGenerator.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil;
 import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
 import edu.wpi.first.wpilibj.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -58,55 +59,61 @@ public class TrajectoryCommandGenerator {
     }
 
     public static List<Command> getTrajectoryCommand(String trajectoryName, DriveSubsystem robotDrive, PIDController leftPIDController, PIDController rightPIDController) {
-        Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve("Paths/" + trajectoryName + ".json");
+        Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve("Paths/" + trajectoryName + ".wpilib.json");
         String trajectoryInfo = "";
         DifferentialDriveVoltageConstraint autoVoltageConstraint = new DifferentialDriveVoltageConstraint(
             robotDrive.feedForward,
             DriveConstants.kDriveKinematics, 10);
         TrajectoryConfig config = new TrajectoryConfig(AutoConstants.kMaxSpeedMetersPerSecond, AutoConstants.kMaxAccelerationMetersPerSecondSquared).setKinematics(DriveConstants.kDriveKinematics)
             .addConstraint(autoVoltageConstraint);
+        // try {
+        //     trajectoryInfo = Files.readString(trajectoryPath);
+        // } catch (IOException e) {
+        //     DriverStation.reportError("Unable to open " + trajectoryName + " path", e.getStackTrace());
+        // }
+        // ObjectMapper mapper = new ObjectMapper();
+        // JsonNode headNode = null;
+        // try {
+        //     headNode = mapper.readTree(trajectoryInfo);
+        // } catch (JsonProcessingException e) {
+        //     DriverStation.reportError("Unable to process " + trajectoryName + " path", e.getStackTrace());
+        // }
+
+        // assert headNode != null;
+        // Iterator<JsonNode> pathsNodeArray = headNode.elements();
+        // List<TrajectoryWaypoints> pathsArray = new ArrayList<>();
+        // pathsNodeArray.forEachRemaining((JsonNode path) -> {
+        //     TrajectoryWaypoints pathObject = new TrajectoryWaypoints();
+        //     pathObject.start.x = path.get("start").get("x").asDouble();
+        //     pathObject.start.y = path.get("start").get("y").asDouble();
+        //     pathObject.start.theta = path.get("start").get("theta").asDouble();
+        //     pathObject.finish.x = path.get("finish").get("x").asDouble();
+        //     pathObject.finish.y = path.get("finish").get("y").asDouble();
+        //     pathObject.finish.theta = path.get("finish").get("theta").asDouble();
+        //     pathObject.reversed = path.get("reversed").asBoolean();
+        //     List<double[]> waypointsList = new ArrayList<>();
+        //     path.get("waypoints").elements().forEachRemaining((JsonNode waypoint) -> {
+        //         double[] waypointArray = {waypoint.get(0).asDouble(), waypoint.get(1).asDouble()};
+        //         waypointsList.add(waypointArray);
+        //     });
+        //     pathObject.waypoints = waypointsList.toArray(new double[waypointsList.size()][2]);
+        //     pathsArray.add(pathObject);
+        // });
+
+        // List<Trajectory> trajectories = pathsArray.stream().map(
+        //         (TrajectoryWaypoints waypointsInfo) -> {
+        //             Pose2d start = new Pose2d(Units.feetToMeters(waypointsInfo.start.x), Units.feetToMeters(waypointsInfo.start.y), Rotation2d.fromDegrees(waypointsInfo.start.theta));
+        //             Pose2d end = new Pose2d(Units.feetToMeters(waypointsInfo.finish.x), Units.feetToMeters(waypointsInfo.finish.y), Rotation2d.fromDegrees(waypointsInfo.finish.theta));
+        //             List<Translation2d> interiorWaypoints = Stream.of(waypointsInfo.waypoints).map((double[] waypoint) -> new Translation2d(Units.feetToMeters(waypoint[0]), Units.feetToMeters(waypoint[1]))).collect(Collectors.toList());
+        //             return TrajectoryGenerator.generateTrajectory(start, interiorWaypoints, end, config.setReversed(waypointsInfo.reversed));
+        //         }
+        // ).collect(Collectors.toList());
+        List<Trajectory> trajectories = null;
         try {
-            trajectoryInfo = Files.readString(trajectoryPath);
+        trajectories = List.of(TrajectoryUtil.fromPathweaverJson(trajectoryPath));
         } catch (IOException e) {
-            DriverStation.reportError("Unable to open " + trajectoryName + " path", e.getStackTrace());
+            DriverStation.reportError(e.getMessage(), e.getStackTrace());
         }
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode headNode = null;
-        try {
-            headNode = mapper.readTree(trajectoryInfo);
-        } catch (JsonProcessingException e) {
-            DriverStation.reportError("Unable to process " + trajectoryName + " path", e.getStackTrace());
-        }
-
-        assert headNode != null;
-        Iterator<JsonNode> pathsNodeArray = headNode.elements();
-        List<TrajectoryWaypoints> pathsArray = new ArrayList<>();
-        pathsNodeArray.forEachRemaining((JsonNode path) -> {
-            TrajectoryWaypoints pathObject = new TrajectoryWaypoints();
-            pathObject.start.x = path.get("start").get("x").asDouble();
-            pathObject.start.y = path.get("start").get("y").asDouble();
-            pathObject.start.theta = path.get("start").get("theta").asDouble();
-            pathObject.finish.x = path.get("finish").get("x").asDouble();
-            pathObject.finish.y = path.get("finish").get("y").asDouble();
-            pathObject.finish.theta = path.get("finish").get("theta").asDouble();
-            pathObject.reversed = path.get("reversed").asBoolean();
-            List<double[]> waypointsList = new ArrayList<>();
-            path.get("waypoints").elements().forEachRemaining((JsonNode waypoint) -> {
-                double[] waypointArray = {waypoint.get(0).asDouble(), waypoint.get(1).asDouble()};
-                waypointsList.add(waypointArray);
-            });
-            pathObject.waypoints = waypointsList.toArray(new double[waypointsList.size()][2]);
-            pathsArray.add(pathObject);
-        });
-
-        List<Trajectory> trajectories = pathsArray.stream().map(
-                (TrajectoryWaypoints waypointsInfo) -> {
-                    Pose2d start = new Pose2d(Units.feetToMeters(waypointsInfo.start.x), Units.feetToMeters(waypointsInfo.start.y), Rotation2d.fromDegrees(waypointsInfo.start.theta));
-                    Pose2d end = new Pose2d(Units.feetToMeters(waypointsInfo.finish.x), Units.feetToMeters(waypointsInfo.finish.y), Rotation2d.fromDegrees(waypointsInfo.finish.theta));
-                    List<Translation2d> interiorWaypoints = Stream.of(waypointsInfo.waypoints).map((double[] waypoint) -> new Translation2d(Units.feetToMeters(waypoint[0]), Units.feetToMeters(waypoint[1]))).collect(Collectors.toList());
-                    return TrajectoryGenerator.generateTrajectory(start, interiorWaypoints, end, config.setReversed(waypointsInfo.reversed));
-                }
-        ).collect(Collectors.toList());
         return getTrajectoryCommand(trajectories, robotDrive, leftPIDController, rightPIDController);
     }
 
