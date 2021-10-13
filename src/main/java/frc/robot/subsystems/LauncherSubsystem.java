3577de13bad9553468dc81cae898d4843b5d7c70
Henry Scheible <henry.scheible@gmail.com>
2021-10-13 12:34:18 -0500

    Update logging in launcher subsystem

--- src/main/java/frc/robot/subsystems/LauncherSubsystem.java
+++ src/main/java/frc/robot/subsystems/LauncherSubsystem.java
@@ -37,18 +37,31 @@ public class LauncherSubsystem extends SubsystemBase implements Loggable {
         BadLog.createValue("Launcher/kP", "" + LauncherConstants.kP);
         BadLog.createValue("Launcher/kD", "" + LauncherConstants.kD);
         BadLog.createValue("Launcher/kI", "" + LauncherConstants.kI);
+        BadLog.createValue("Launcher/kS", "" + LauncherConstants.kS);
+        BadLog.createValue("Launcher/kV", "" + LauncherConstants.kV);
+        BadLog.createValue("Launcher/kA", "" + LauncherConstants.kA);
 
-        BadLog.createTopic("Launcher/Target Velocity", "rpm", pidController::getSetpoint, "join: Launcher/Launcher PID Loop");
-        BadLog.createTopic("Launcher/Error", "rpm", pidController::getPositionError, "join: Launcher/Launcher PID Loop");
-        BadLog.createTopicSubscriber("Launcher/Launcher Velocity", "rpm", DataInferMode.DEFAULT, "join: Launcher/Launcher PID Loop");
+        BadLog.createTopic("Launcher/Target Velocity", "rpm", pidController::getSetpoint, "join:Launcher/Launcher PID Loop");
+        BadLog.createTopic("Launcher/Error", "rpm", pidController::getPositionError, "join:Launcher/Launcher PID Loop");
+        BadLog.createTopicSubscriber("Launcher/Launcher Velocity", "rpm", DataInferMode.DEFAULT, "join:Launcher/Launcher PID Loop");
+        BadLog.createTopicSubscriber("Launcher/Launcher Feedforward Voltage", "volts", DataInferMode.DEFAULT, "join:Launcher/Launcher PID Loop");
+        BadLog.createTopicSubscriber("Launcher/Launcher Feedback Voltage", "volts", DataInferMode.DEFAULT, "join:Launcher/Launcher PID Loop");
+        BadLog.createTopicSubscriber("Launcher/Total Voltage", "volts", DataInferMode.DEFAULT, "join:Launcher/Launcher PID Loop");
+
     }
 
 
     // speed is in flywheel rpm
     public void spinLauncher(double speed) {
-        double currentRPM = 10 * master.getSelectedSensorVelocity(0) / LauncherConstants.kEncoderUnitsPerRevolution;
+        double currentRPM = master.getSelectedSensorVelocity(0) * LauncherConstants.kRawVelocityToRPM;
         BadLog.publish("Launcher/Launcher Velocity", currentRPM);
-        double motorVoltage = pidController.calculate(currentRPM, speed);//+feedforward.calculate(speed);
+        double feedforwardVoltage = feedforward.calculate(speed);
+        double feedbackVoltage = pidController.calculate(currentRPM, speed);
+        double motorVoltage = feedbackVoltage + feedforwardVoltage;
+        BadLog.publish("Launcher/Launcher Feedforward Voltage", feedforwardVoltage);
+        BadLog.publish("Launcher/Launcher Feedback Voltage", feedbackVoltage);
+        BadLog.publish("Launcher/Total Voltage", motorVoltage);
+
         master.setVoltage(motorVoltage);
         follower.feed();
         SmartDashboard.putNumber("Launcher/Target Velocity", pidController.getSetpoint());
